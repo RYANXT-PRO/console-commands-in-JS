5ac6e7423fa4f7daa93ad88831d46ebfb47a4226
RYANXT-PRO <127141211+RYANXT-PRO@users.noreply.github.com>
2023-03-12 12:47:15 +0530

    Add files via upload

---
+++ practice.js
@@ -0,0 +1,34 @@
+ var a = 10;
+ var b = 8 ;
+ var c = a+b ;
+ 
+ console.log(c +5 ); // ans. in console is 23
+ 
+ var x = [11,12,13,14,15]; 
+ console.log(x);  // ans. in console is (5){11,12,13,14,15}
+ 
+ var y = [11,12,13,14,15];
+ console.table(y);  //  ans. in table form 
+
+ // this is console error command which execute in console 
+ 
+ console.error("something went wrong in file ");  //  ans. in console print as it is 
+
+ // this is console warn which use to give warning in console 
+ console.warn("it is just a warning 1 "); // ans. in console print as it is 
+ console.warn("it is just a warning 2");
+ console.warn("it is just a warning 3");
+ console.warn("it is just a warning 4");
+ console.warn("it is just a warning 5");
+ console.warn("it is just a warning 6");
+
+ console.time();    // this console use for to find how much programme take time to execute 
+ console.warn("it is just a warning "); 
+ console.warn("it is just a warning ");
+ console.warn("it is just a warning ");
+ console.warn("it is just a warning ");
+ console.warn("it is just a warning ");
+ console.warn("it is just a warning ");
+ console.timeEnd();
+
+ console.clear(); // use for clear all recent command in console 
